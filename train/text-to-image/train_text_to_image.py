51e372b3f86a9d1e7a99c250b34f797cef2cda2f
haandol <ldg55d@gmail.com>
2023-11-01 15:49:15 +0900

    set xformers default

--- train/text-to-image/train_text_to_image.py
+++ train/text-to-image/train_text_to_image.py
@@ -356,7 +356,7 @@ def parse_args():
     parser.add_argument(
         "--enable_xformers_memory_efficient_attention",
         type=str2bool,
-        default=False,
+        default=True,
         help="Whether or not to use xformers."
     )
     parser.add_argument(
