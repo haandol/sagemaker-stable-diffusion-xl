6af1526607b132ef009aa2a630121f556b7786a0
haandol <ldg55d@gmail.com>
2024-01-27 15:42:41 +0900

    feat: add log for xformers

--- train/text-to-image/sdxl-lora-sagemaker.py
+++ train/text-to-image/sdxl-lora-sagemaker.py
@@ -616,6 +616,7 @@ def main(args):
                     "xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details."
                 )
             unet.enable_xformers_memory_efficient_attention()
+            logger.info("xFormers memory efficient attention enabled.")
         else:
             raise ValueError(
                 "xformers is not available. Make sure it is installed correctly"
